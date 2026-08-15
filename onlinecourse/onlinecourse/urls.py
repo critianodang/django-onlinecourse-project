8bed79df69fab1a58fa161aa5925500d031bc509
Nguyen Phan Nhat Dang <nnhatdang365@gmail.com>
2026-08-15 16:19:37 +0700

    Add URL patterns for online course app

---
+++ onlinecourse/onlinecourse/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'onlinecourse'
+urlpatterns = [
+    path('', views.CourseListView.as_view(), name='index'),
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
+    path('<int:course_id>/enroll/', views.enroll, name='enroll'),
+    
+    # Paths for exam submit and show result
+    path('<int:course_id>/submit/', views.submit, name='submit'),
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
+]
